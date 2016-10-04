1a30a005ee685a270cc896a62eafe464e2aa5f3e
imoto9610 <kd1189133@kobedenshi.ac.jp>
2016-10-04 16:35:38 +0900

    add Intent filter

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.project;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+            //NFCがタッチされた時にする処理
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("テストメッセージ")
+                    .setPositiveButton("終了", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // ボタンをクリックしたときの動作
+                            finish();
+                        }
+                    });
+            builder.show();
+
+        }else {
+            finish();
+        }
+
     }
 }
