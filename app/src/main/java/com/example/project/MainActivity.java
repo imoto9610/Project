85f365efc7c7f6ca60dab640f639d91c47369c1c
imoto9610 <kd1189133@kobedenshi.ac.jp>
2016-10-06 13:16:17 +0900

    customize style

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
             builder.show();
 
         }else {
-            finish();
+            setContentView(R.layout.activity_main);
         }
 
     }
